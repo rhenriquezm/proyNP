c4bdb75dbe866646490c5888016e60a029bc468a
Rodrigo Ignacio Henriquez Moreno <Rodrigo Ignacio Henriquez Moreno>
2017-03-17 15:42:27 -0300

    Buscar Servicio

---
+++ apiNP/datos/login_mysqlServicio.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOST", "localhost"); // Nombre del host
+define("BD", "nimbusplace"); // Nombre de la base de controladores
+define("USER", "root"); // Nombre del usuario
+define("CON", ""); // Constraseña
